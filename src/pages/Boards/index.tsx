548771bc9cb9d823e374d8f46e774a1cffd01191
Tan Suan Khai <mail.abcwxyz@gmail.com>
2024-03-31 15:21:04 +0800

    Add board component

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -1,5 +1,55 @@
-const Boards = () => {
-    return <div>Boards</div>
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { Columns } from "../../types";
+import { Board } from "../../data/board";
+import { AddOutline } from "react-ionicons";
+import Task from "../../components/Task";
 
-export default Boards
+const Boards = () => {
+  const [columns, setColumns] = useState<Columns>(Board);
+  return (
+    <>
+      <DragDropContext onDragEnd={(result: any) => console.log(result)}>
+        <div className="w-full flex items-start justify-between px-5 pb-8 md:gap-0 gap-10">
+          {Object.entries(columns).map(([columnId, column]: any) => (
+            <div key={columnId} className="w-full flex flex-col">
+              <Droppable droppableId={columnId} key={columnId}>
+                {(provided: any) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="flex flex-col md:w-[90%] w-[250px] gap-3 items-center py-5"
+                  >
+                    <div className="flex items-center justify-center py-[10px] w-full bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]">
+                      {column.name}
+                    </div>
+                    {column.items.map((task: any, index: any) => {
+                      <Draggable
+                        key={task.id.toString()}
+                        draggableId={task.id.toString()}
+                        index={index}
+                      >
+                        {(provided: any) => (
+                          <>
+                            <Task />
+                          </>
+                        )}
+                      </Draggable>;
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+              <div className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]">
+                <AddOutline color={"#555"} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </DragDropContext>
+    </>
+  );
+};
+
+export default Boards;
