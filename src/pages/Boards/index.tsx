a98ba610501bae91c9823b53675db7951f9639f3
Tan Suan Khai <mail.abcwxyz@gmail.com>
2024-03-31 21:51:32 +0800

    Add modal component

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -5,12 +5,33 @@ import { Columns } from "../../types";
 import { Board } from "../../data/board";
 import { AddOutline } from "react-ionicons";
 import Task from "../../components/Task";
+import AddModal from "../../components/Modals/AddModal";
+import { onDragEnd } from "../../helpers/onDragEnd";
 
 const Boards = () => {
   const [columns, setColumns] = useState<Columns>(Board);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedColumn, setSelectedColumn] = useState("");
+
+  const openModal = (columnId: any) => {
+    setSelectedColumn(columnId);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleAddTask = (taskData: any) => {
+    const newBoard = { ...columns };
+    newBoard[selectedColumn].items.push(taskData);
+  };
+
   return (
     <>
-      <DragDropContext onDragEnd={(result: any) => console.log(result)}>
+      <DragDropContext
+        onDragEnd={(result: any) => onDragEnd(result, columns, setColumns)}
+      >
         <div className="w-full flex items-start justify-between px-5 pb-8 md:gap-0 gap-10">
           {Object.entries(columns).map(([columnId, column]: any) => (
             <div key={columnId} className="w-full flex flex-col">
@@ -32,10 +53,7 @@ const Boards = () => {
                       >
                         {(provided: any) => (
                           <>
-                            <Task
-                                provided={provided}
-                                task={task}
-                            />
+                            <Task provided={provided} task={task} />
                           </>
                         )}
                       </Draggable>;
@@ -44,13 +62,24 @@ const Boards = () => {
                   </div>
                 )}
               </Droppable>
-              <div className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]">
+              <div
+                onClick={() => openModal(columnId)}
+                className="flex cursor-pointer items-center justify-center gap-1 py-[10px] md:w-[90%] w-full opacity-90 bg-white rounded-lg shadow-sm text-[#555] font-medium text-[15px]"
+              >
                 <AddOutline color={"#555"} />
+                Add Task
               </div>
             </div>
           ))}
         </div>
       </DragDropContext>
+
+      <AddModal
+        isOpen={modalOpen}
+        onClose={closeModal}
+        setOpen={setModalOpen}
+        handleAddTask={handleAddTask}
+      />
     </>
   );
 };
