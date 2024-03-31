e3c118fe4b5ae08550c0a456d6d711e33cd2aace
Tan Suan Khai <mail.abcwxyz@gmail.com>
2024-04-01 00:48:50 +0800

    Update navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,41 +1,25 @@
-import { ChevronDown, NotificationsOutline, PersonOutline, SearchOutline, SettingsOutline, ShareSocialOutline } from "react-ionicons";
+import { SearchOutline, TodayOutline } from "react-ionicons";
 
 const Navbar = () => {
   return (
-    <div className="md:w-[calc(100%-230px)] w-[calc(100%-60px)] fixed flex items-center justify-between pl-2 pr-6 h-[70px] top-0 md:left-[230px] left-[60px] border-b border-slate-300 bg-[#fff]">
-      <div className="flex items-center gap-3 cursor-pointer">
-        <PersonOutline
+    <div className="w-full fixed flex flex-col md:flex-row items-center gap-2 justify-between pb-5 md:pb-0 px-5 h-[12vh] md:h-[10vh] top-0 border-b border-slate-300 bg-[#fff]">
+      <div className="flex items-center gap-3">
+        <TodayOutline
             color="#fb923c"
             width={"28px"}
             height={"28px"}
         />
         <span className="text-orange-400 font-semibold md:text-lg text-sm whitespace-nowrap">
-            Board Name
+            Task Board
         </span>
-        <ChevronDown
-            color="#fb923c"
-            width={"16px"}
-            height={"16px"}
-        />
       </div>
-      <div className="flex items-center gap-2 md:w-[800px] bg-gray-100 rounded-lg px-3 py-[10px]">
+      <div className="flex items-center gap-2 w-full bg-gray-100 rounded-lg px-3 py-[10px]">
         <SearchOutline color={"#999"} />
         <input
             type="text"
             placeholder="Search"
             className="w-full bg-gray-100 outline-none text-[15px]"
         />
-      </div>
-      <div className="md:flex hidden items-center gap-4">
-        <div className="grid place-items-center bg-gray-100 rounded-full p-2 cursor-pointer">
-            <ShareSocialOutline color={"#444"} />
-        </div>
-        <div className="grid place-items-center bg-gray-100 rounded-full p-2 cursor-pointer">
-            <SettingsOutline color={"#444"} />
-        </div>
-        <div className="grid place-items-center bg-gray-100 rounded-full p-2 cursor-pointer">
-            <NotificationsOutline color={"#444"} />
-        </div>
       </div>
     </div>
   );
